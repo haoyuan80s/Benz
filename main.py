71e6ae0eeab6d4d0655c10ce3f760128930d5933
Hao Yuan <haoyuan80s@gmail.com>
2017-06-15 16:25:24 -0700

    To turning Paras[1]

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ from sklearn.preprocessing import LabelEncoder
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+import xgboost as xgb
+from sklearn.decomposition import PCA, FastICA
+from sklearn.metrics import r2_score
+
 def main():
-    favorite_color = { "lion": "yellow", "kitty": "red" }
-    pickle.dump( favorite_color, open( "save.p", "wb" ) )
-
-    pickle.load( open( "save.p", "rb" ) )
-
     # read datasets
     train = pd.read_csv('data_fun/data/train.csv')
     test = pd.read_csv('data_fun/data/test.csv')
@@ -26,7 +25,7 @@ def main():
     print('Shape train: {}\nShape test: {}'.format(train.shape, test.shape))
 
     # This step is huge!
-    from sklearn.decomposition import PCA, FastICA
+    
     n_comp = 10
 
     # PCA
@@ -52,7 +51,7 @@ def main():
     y_train = train["y"]
     y_mean = np.mean(y_train)
     ()# mmm, xgboost, loved by everyone ^-^
-    import xgboost as xgb
+    
     xgb_params = {
         'n_estimators': 550,
         'learning_rate': 0.005,
@@ -61,48 +60,49 @@ def main():
         'objective': 'reg:linear',
         #'eval_metric': 'rmse',
         'base_score': y_mean, # base prediction = mean(target)
-        #'silent': 1
+        'silent': 0
     }
+    def grid_search():
+        boost = xgb.XGBRegressor(**xgb_params)
+        parameters = {
+            # n_estimators
+            'learning_rate': [0.01, 0.015,0.05],
+            'gamma': [0,0.1,0.5,0.9],
+            'max_depth': [4, 9],
+            'min_child_weight': [1,5],
+            "subsample": [0.6,0.8,1],
+            'colsample_bytree': [0.6,0.8,1],
+            'reg_alpha' : [0],
+            'reg_lambda' : [1],
+        }
+        reg = GridSearchCV(boost, parameters, n_jobs=1, cv=3, verbose = 0)
+        reg.fit(train.drop('y', axis=1).as_matrix(), y_train)
 
-    boost = xgb.XGBRegressor(**xgb_params)
+        best_parameters, score, _ = max(reg.grid_scores_, key=lambda x: x[1])
+        print(score)
+        for param_name in sorted(best_parameters.keys()):
+            print("%s: %r" % (param_name, best_parameters[param_name]))
 
-    parameters = {
-        # n_estimators
-        'learning_rate': [0.01, 0.015,0.05],
-        'gamma': [0,0.1,0.5,0.9],
-        'max_depth': [4, 9],
-        'min_child_weight': [1,5],
-        "subsample": [0.6,0.8,1],
-        'colsample_bytree': [0.6,0.8,1],
-        'reg_alpha' : [0],
-        'reg_lambda' : [1],
-    }
-    reg = GridSearchCV(boost, parameters, n_jobs=1, cv=3, verbose = 2)
-    reg.fit(train.drop('y', axis=1).as_matrix(), y_train)
-
-    best_parameters, score, _ = max(reg.grid_scores_, key=lambda x: x[1])
-    print(score)
-    for param_name in sorted(best_parameters.keys()):
-        print("%s: %r" % (param_name, best_parameters[param_name]))
-
-    pickle.dump( reg.best_params_, open("bestpara.p", "wb" ))
-
+        pickle.dump( reg.best_params_, open("bestpara.p", "wb" ))
+        return reg.best_params_
+    
+    do_grid_search = False
+    if do_grid_search:
+        xgb_params = grid_search()
 
     dtrain = xgb.DMatrix(train.drop('y', axis=1), y_train)
     dtest = xgb.DMatrix(test)
 
-
     def r2_metric(preds, dtrain):
         """Self defined evaluation obj"""
-        from sklearn.metrics import r2_score
         return 'r2_metric', r2_score(dtrain.get_label(), preds)
                 
     # xgboost, cross-validation
-    cv_result = xgb.cv(reg.best_params_,
+    cv_result = xgb.cv(xgb_params,
                        dtrain,
                        num_boost_round=1000, # increase to have better results (~700)
                        early_stopping_rounds=50,
-                       verbose_eval=50,
+                       verbose_eval=1,
                        show_stdv=False,
                        feval = r2_metric,
                        maximize = True
@@ -112,7 +112,7 @@ def main():
     print(num_boost_rounds)
 
     # train model
-    model = xgb.train(dict(reg.best_params_, silent=0), dtrain, num_boost_round=num_boost_rounds)
+    model = xgb.train(dict(xgb_params, silent=0), dtrain, num_boost_round=num_boost_rounds)
 
 
     # check f2-score (to get higher score - increase num_boost_round in previous cell)
@@ -125,5 +125,5 @@ def main():
     output = pd.DataFrame({'id': test['ID'].astype(np.int32), 'y': y_pred})
     output.to_csv('xgboost-depth{}-pca-ica.csv'.format(xgb_params['max_depth']), index=False)
 
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+main()
